f090c50eeaad633df704c24d03be74f676798ba5
Travis <travisformayor@users.noreply.github.com>
2021-07-28 04:22:25 -0700

    break out update into function

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,63 +1,88 @@
+
+import { BigInt, Address } from "@graphprotocol/graph-ts"
+import { CurioUser, CardBalance, SetBalance, } from "../generated/schema"
 import {
+  CurioERC1155Wrapper as WrapperContract,
   TransferSingle as TransferSingleEvent,
   TransferBatch as TransferBatchEvent
 } from "../generated/CurioERC1155Wrapper/CurioERC1155Wrapper"
-// import { CurioCollector, CardBalance, SetBalance, } from "../generated/schema"
+
+// == Possible Event Transaction Types == //
+// Wrapping Event:  
+//   _operator = user && same as _to
+//   _from = 0x0 (burn) 
+//   _to = user
+// Unwrapping Event:
+//   _operator = user
+//   _from = Contract
+//   _to = 0x0 (burn)
+// Transfer Event:
+//   _operator = user && same as _from
+//   _from = user
+//   _to = a different user
+
+function updateUserCardBalance(
+  eventUser: Address, 
+  eventCardId: BigInt, 
+  contractLookup: WrapperContract
+) {
+    // Load or Create user record
+    let user = CurioUser.load(eventUser.toHex());
+    if (!user) user = new CurioUser(eventUser.toHex());
+    // to do: a .save()
+    
+    // Load or Create record of users balance for the card involved in the event
+    const userCardId = `${eventUser.toHex()}-${eventCardId.toString()}`;
+    let userCardBalance = CardBalance.load(userCardId);
+    if (!userCardBalance) userCardBalance = new CardBalance(userCardId)
+
+    // Update user balance for card involved in event
+    userCardBalance.cardNumber = eventCardId;
+    userCardBalance.balance = contractLookup.balanceOf(eventUser, eventCardId);
+    userCardBalance.save();
+
+    // Associate the userCardBalance with the users record
+    // If it's already associated there is no need to update the data, 
+    // (cont.) it is a reference to the CardBalance that was updated
+    if (user.cards.indexOf(userCardBalance.id) == -1) {
+      // -1 means this userCardBalance id has not been assoicated yet (first time)
+      user.cards.push(userCardBalance.id);
+    }
+
+    // to do: function to add unique number and sets
+    user.save();
+}
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
 
   const burnAddr:string = "0x0000000000000000000000000000000000000000";
   const contractAddr:string = "0x73DA73EF3a6982109c4d5BDb0dB9dd3E3783f313";
 
-  // == Possible Event Transaction Types == //
-  // Wrapping Event:  
-  //   _operator = user && same as _to
-  //   _from = 0x0 (burn) 
-  //   _to = user
-  // Unwrapping Event:
-  //   _operator = user
-  //   _from = Contract
-  //   _to = 0x0 (burn)
-  // Transfer Event:
-  //   _operator = user && same as _from
-  //   _from = user
-  //   _to = a different user
-  const eventOperator = event.params._operator.toHex(); // addr that executed transfer
-  const eventFrom = event.params._from.toHex();
-  const eventTo = event.params._to.toHex();
-  const eventCard = event.params._id.toHex();
-  const eventQuantity = event.params._value.toHex();
+  const eventOperator = event.params._operator; // addr that executed transfer
+  const eventFrom = event.params._from;
+  const eventTo = event.params._to;
+  const eventCard = event.params._id;
+  // const eventQuantity = event.params._value;
+  
+  // Wrapper Contract object to access public state
+  const contractLookup = WrapperContract.bind(event.address);
 
-  if (eventOperator == eventTo && eventFrom == burnAddr ) {
+  if (eventOperator.toHex() == eventTo.toHex() && eventFrom.toHex() == burnAddr ) {
     // Wrap Event
     // Check and update balance of eventTo
-  } else if (eventFrom == contractAddr && eventTo == burnAddr) {
+    updateUserCardBalance(eventTo, eventCard, contractLookup);
+  } else if (eventFrom.toHex() == contractAddr && eventTo.toHex() == burnAddr) {
     // Unwrap Event
     // Check and update balance of eventOperator
+    updateUserCardBalance(eventOperator, eventCard, contractLookup);
   } else if (eventOperator == eventFrom) {
     // Normal Transfer Event
     // Check and update balance of eventFrom and eventTo
+    updateUserCardBalance(eventFrom, eventCard, contractLookup);
+    updateUserCardBalance(eventTo, eventCard, contractLookup);
   }
-
-  // let entity = new TransferSingle(
-  //   event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  // )
-  // entity._operator = event.params._operator
-  // entity._from = event.params._from
-  // entity._to = event.params._to
-  // entity._id = event.params._id
-  // entity._value = event.params._value
-  // entity.save()
 }
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
-  // let entity = new TransferBatch(
-  //   event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  // )
-  // entity._operator = event.params._operator
-  // entity._from = event.params._from
-  // entity._to = event.params._to
-  // entity._ids = event.params._ids
-  // entity._values = event.params._values
-  // entity.save()
+  // to do
 }
